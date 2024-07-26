02b39000f7b8be279ba342186ce2452b3f42aad4
john1015 <10151404john@gmail.com>
2024-07-26 17:10:48 +0900

    111111

--- DEF/src/main/java/com/sist/main/MainClass.java
+++ DEF/src/main/java/com/sist/main/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("asdad");
 		System.out.println("Siuuuuuuuuuuuuuuuuuuuuuuuu");
+		System.out.println("Siuuuuuuuuuuuuuuuuuuuuuuuuasdasdsaddas");
 	}
 
 }
